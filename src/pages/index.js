9100652f694ab21efe4a2e94c38d3d19904ae9e5
Hamid Hassani <hamidhassani119@gmail.com>
2023-03-04 13:13:06 +0330

    Some changes occurred!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import FormComponent from "@/components/form";
 import Todos from "./todos/todos";
 import { useSWRConfig } from "swr";
